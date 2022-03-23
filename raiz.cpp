ad0119fd99301fdb5509088372df36a3a5b9508c
Jair Herrera <gedrysjair@hotmail.com>
2022-03-23 11:53:50 -0600

    tarea 8

---
+++ raiz.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    double num1 = 20;
+    double num2 = 0;
+    
+    num2= sqrt(num1);
+    
+    cout<<"\nEl numero "<<num1<<"   \ntiene como raiz cuadrada: "<<num2<<endl;
+    
+    return 0;
+}
